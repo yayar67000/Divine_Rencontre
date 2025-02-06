59932ca114d7c8fc82f4e63f8bc3007259542765
Victoria Fonteny <victoriafonteny@live.fr>
2025-02-06 13:54:29 +0100

    creat page profil

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,10 +5,10 @@ export default function Navbar() {
 	return (
 		<nav>
 			{/* css a faire  */}
-			<NavLink to="/" className="nav-link-lat">
+			<NavLink to="/profil" className="nav-link-lat">
 				<img src="/vieil-homme.png" alt="" />
 			</NavLink>
-			<NavLink to="" className="nav-link-lat">
+			<NavLink to="/" className="nav-link-lat">
 				<img src="eclat.png" alt="" />
 			</NavLink>
 			<NavLink to="/messages" className="nav-link-lat">
