c3304b5244d0272a4d2ce4a1beefa44987aab1f0
Jon33260 <j.mallet82@laposte.net>
2025-02-06 14:03:09 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import "./App.css";
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
-import "./pages/messages.css";
 import { Outlet } from "react-router-dom";
 
 function App() {
 	return (
-		<>
-			<Navbar />
-			<h1> Divine rencontre</h1>
-			<Outlet />
-			<Footer />
-		</>
+	<>
+	<main>
+      <Outlet />
+    </main>
+	</>
 	);
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,40 @@
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.tsx";
-import Messages from "./pages/messages.tsx";
+import Profil from "./Pages/Profil.tsx";
+import Home from "./Pages/Home.tsx";
+// import Messages from "./Pages/Messages.tsx";
 
-// Router creation
+
 const router = createBrowserRouter([
 	{
-		element: <App />,
-		children: [
-			{
-				path: "/",
-				// element: < />, Mettre la page home
-			},
-			{
-				path: "/messages",
-				element: <Messages />,
-			},
-		],
-	},
-]);
-
+	  // The root path
+	  element: <App />, // Renders the App component for the home page
+	  children: [
+		{
+			path: "/",
+			element: <Home />,
+		},
+		{
+			path: "/profil",
+			element: <Profil />,
+		  },
+		//   {
+		// 	path: "/messages",
+		// 	element: <Messages />,
+		//   },
+		]
+	  }
+	]);
 const rootElement = document.getElementById("root");
-
-if (rootElement != null) {
-	ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+if (rootElement == null) {
+	throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
 }
+createRoot(rootElement).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
