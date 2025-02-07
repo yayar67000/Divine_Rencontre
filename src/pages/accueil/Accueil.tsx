0f80732641e20a0aedfb4fc6ed17f6c154879f6f
Jon33260 <j.mallet82@laposte.net>
2025-02-07 10:31:27 +0100

    page accueil ok

---
+++ src/pages/accueil/Accueil.tsx
@@ -0,0 +1,16 @@
+import "./accueil.css";
+export default function Accueil() {
+	return (
+		<>
+			<h1 className="welcome">Bienvenue sur "Divine rencontre" !</h1>
+			<img className="img-accueil" src="./Olympe.jpg" alt="L'olympe" />
+			<p className="Message-accueil">
+				Ici, les dieux et déesses grecs se rencontrent pour tisser des liens
+				célestes. Que vous soyez un Titan cherchant à briser des chaînes ou un
+				dieu en quête de la muse parfaite, vous trouverez votre moitié divine
+				parmi les immortels. Prêt à faire briller l’amour sous le regard des
+				étoiles ? Rejoignez-nous dès aujourd’hui !
+			</p>
+		</>
+	);
+}
