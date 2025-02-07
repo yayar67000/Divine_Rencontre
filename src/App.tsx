26d93ba6779bba349f7b9d86ba10be0177214ccc
Jon33260 <j.mallet82@laposte.net>
2025-02-07 09:18:02 +0100

    footer ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
 	return (
+		<>
 		<main>
 			{" "}
 			<Navbar />
 			<Outlet />
-			<Footer />
 		</main>
+		<Footer />
+		</>
 	);
 }
 
