2af0a2060674a707f790afda909c50f22ae4a24c
Victoria Fonteny <victoriafonteny@live.fr>
2025-02-07 10:00:44 +0100

    dematch goodé

--- src/pages/Profil_Details/ProfilDetails.tsx
+++ src/pages/Profil_Details/ProfilDetails.tsx
@@ -1,6 +1,9 @@
 import "./ProfilDetails.css";
 import DataProfils from "../../services/DataProfils";
 import { useParams } from "react-router-dom";
+import { useMatchedGods } from "../../context/MatchContext";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 interface ProfilType {
 	id: number;
@@ -13,27 +16,53 @@ interface ProfilType {
 
 export default function ProfilDetails() {
 	const profils = DataProfils as ProfilType[];
+	const [dematch, setDematch] = useState(false);
+
+	const { matchedGods, setMatchedGods } = useMatchedGods(); // Utilisation du contexte
+
 	const { id } = useParams();
 	const profil = profils.find((projet) => projet.id === Number(id));
 	if (!profil) {
 		return <p>Profil non trouvé</p>;
 	}
 
+	const handleClickDematch = () => {
+		setDematch(true);
+		alert(`Tu viens de supprimer ${profil.nom} de tes matchs !`);
+		setMatchedGods((prevMatches) =>
+			prevMatches.filter((god) => god.id !== profil.id),
+		);
+	};
+
 	return (
-		<section className="detailprofil_content">
-			<img className="profil_image" src={profil.image} alt={profil.nom} />
-			<h2 className="name">{profil.nom}</h2>
-			<div className="text">
-				<p>
-					<u>Age</u> : {profil.age} ans
-				</p>
-				<p>
-					<u>Description</u> : {profil.description}
-				</p>
-				<p>
-					<u>Recherche</u> : {profil.relation}
-				</p>
+		<>
+			<section className="detailprofil_content">
+				<img className="profil_image" src={profil.image} alt={profil.nom} />
+				<h2 className="name">{profil.nom}</h2>
+				<div className="text">
+					<p>
+						<u>Age</u> : {profil.age} ans
+					</p>
+					<p>
+						<u>Description</u> : {profil.description}
+					</p>
+					<p>
+						<u>Recherche</u> : {profil.relation}
+					</p>
+				</div>
+			</section>
+			<div className="button-profilDetails">
+				<button
+					className={dematch ? "button-dematch" : ""}
+					type="button"
+					onClick={handleClickDematch}
+				>
+					{" "}
+					Dématched ? 🥺{" "}
+				</button>
+
+				<Link to="/messages">Retour aux messages ?</Link>
 			</div>
-		</section>
+		</>
 	);
 }
